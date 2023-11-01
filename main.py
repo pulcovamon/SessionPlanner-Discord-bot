96ae0aa897cbd5772a0b3738abfd4e777ef41b96
pulcovamon <pulcovamon@gmail.com>
2023-11-01 23:27:10 +0100

    bot reads message from discord channel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import os
+from datetime import date
 
-intents = discord.Intents(messages=True, guilds=True)
-client = discord.Client(intents=intents)
+from modules import SessionPlanner
+
+client = discord.Client(intents=discord.Intents(messages=True, guilds=True, message_content=True))
 
 
 @client.event
@@ -14,7 +16,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    print(message.content)
+    if message.content.startswith('$plan session'):
+        message_text = message.content.strip().split(" ")
+        if "from" in message_text:
+            from_index = message_text.index("from")
+            from_date = date(*[int(message_text[i].replace(".", "")) for i in range(from_index+3, from_index, -1)])
+        else:
+            from_date = None
+        if "to" in message_text:
+            to_index = message_text.index("to")
+            to_date = date(*[int(message_text[i].replace(".", "")) for i in range(to_index+3, to_index, -1)])
+        else:
+            to_date = None
+        planner = SessionPlanner(from_date, to_date)
+        print(planner)
         await message.channel.send('Hello!')
 
 client.run(os.getenv('DISCORD_TOKEN'))
--- modules.py
+++ modules.py
@@ -1,16 +1,23 @@
 from datetime import date, timedelta
 
 class SessionPlanner:
-    def __init__(self, from_date=date.today(), to_date=date.today+timedelta(days=7)):
+    def __init__(self, from_date, to_date):
         """_summary_
 
         Args:
             from_date (_type_, optional): _description_. Defaults to date.today().
             to_date (_type_, optional): _description_. Defaults to date.today+timedelta(days=7).
         """
+        if not from_date:
+            from_date = date.today()
+        if not to_date:
+            to_date = from_date + timedelta(days=7)
         self.calendar = []
         delta = timedelta(days=1)
         
         while from_date <= to_date:
             self.calendar.append(from_date)
             from_date += delta
+            
+    def __str__(self) -> str:
+        return f"from: {self.calendar[0]}, to: {self.calendar[-1]}"
