59e17f462ff42d09a3b9c7f490729d8434e2d008
pulcovamon <pulcovamon@gmail.com>
2023-11-05 00:55:41 +0100

    logging added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import logging
+import logging.handlers
 
 from planner import SessionPlanner
 from buttons import ButtonView
@@ -46,5 +47,19 @@ async def plan(ctx, *args):
 
     
 if __name__ == "__main__":
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger('discord.http').setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     bot.run(os.getenv('DISCORD_TOKEN'))
     planner = SessionPlanner()
