e11a511c151e29cadb879bdbcb00d4a188eb90b1
pulcovamon <pulcovamon@gmail.com>
2023-11-05 00:12:02 +0100

    refactored code and docstrings and typing added

--- main.py
+++ main.py
@@ -1,40 +1,150 @@
-from typing import Optional
 import discord
 from discord.ext import commands
 import os
-import asyncio
 from datetime import date, datetime
 import logging
-import interactions
 import re
-
-from modules import SessionPlanner
+from datetime import date, timedelta
+from typing import Tuple, List
 
 CONFIRMED = set()
 DECLINED = set()
 MAYBE = set()
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="$", case_insensitive=True, strip_after_prefix=True, intents=intents)
+
+class SessionPlanner(object):
+    """Allow to create session message and voting message.
+    """
+    def __new__(cls):
+        """It is singleton.
+
+        Returns:
+            SessionPlanner: Instance of this class.
+        """
+        if not hasattr(cls, "instance"):
+            cls.instance = super(SessionPlanner, cls).__new__(cls)
+        return cls.instance
+    
+    async def _format_date(self, argument: str, *args: Tuple[str]) -> datetime.date:
+        """Get date from message text.
+
+        Args:
+            argument (str): Which argument specifies if it is 'start date' of 'end date'.
+
+        Returns:
+            datetime.date: date in right format.
+        """
+        current_date = re.findall(r'\d+', args[args.index(argument)+1])
+        current_date.reverse()
+        current_date = date(*[int(i) for i in current_date])
+        return current_date
+
+    async def set_calendar(self, *args: Tuple[str]):
+        """Set start date and end date of the voting calendar.
+        """
+        if "--from" in args:
+            from_date = await self._format_date("--from", *args)
+        else:
+            from_date = date.today() + timedelta(days=1)
+        if "--to" in args:
+            to_date = await self._format_date("--to", *args)
+        else:
+            to_date = from_date + timedelta(days=7)
+        self.calendar = []
+        delta = timedelta(days=1)
+        while from_date <= to_date:
+            self.calendar.append(from_date)
+            from_date += delta
+
+    async def get_calendar(self) -> List[discord.Embed]:
+        """Return all days from the calendar in message format.
+
+        Returns:
+            List[discord.Embed]: Objects, which can be used to send message.
+        """
+        if not self.calendar:
+            return None
+        embeds = []
+        space = "\u00A0"
+        for day in self.calendar:
+            day_text = f"{day.strftime('%A')}{space}{day.day}.{space}{day.month}.{space}{day.year}"
+            embeds.append(discord.Embed(description=day_text, color=discord.Color.dark_purple()))
+        return embeds
+    
+    async def set_session(self, *args: Tuple[str]) -> bool:
+        """Sets session variables like date, name and place.
+
+        Returns:
+            bool: It returns False if session cannot be set due to no date in args.
+        """
+        if "--date" in args:
+            self.session_date = args[args.index("--date")+1]
+        else:
+            return False
+        if "--name" in args:
+            self.session_name = args[args.index("--name")+1]
+        else:
+            self.session_name = "Next session"
+        if "--place" in args: 
+            self.session_place = args[args.index("--place")+1]
+        else:
+            self.session_place = "Somewhere"
+        return True
+        
+    async def get_session(self) -> discord.Embed:
+        """Return session characteristics in proper format.
+
+        Returns:
+            discord.Embed: Session characteristics, which can be send in message.
+        """
+        embed = discord.Embed(title=self.session_name, description="Are you going?", color=discord.Color.dark_purple())
+        embed.add_field(name="When:", value=self.session_date, inline=False)
+        embed.add_field(name="Where:", value=self.session_place, inline=False)
+        embed.add_field(name="Confirmed:", value="", inline=False)
+        embed.add_field(name="Declined:", value="", inline=False)
+        embed.add_field(name="Maybe:", value="", inline=False)
+        return embed
+
 class ButtonView(discord.ui.View):
-    def __init__(self, embed):
+    """Allow to add buttons to a message.
+    """
+    def __init__(self, embed: discord.Embed):
+        """Not a singleton.
+
+        Args:
+            embed (discord.Embed): Object, which can be sent in message.
+        """
         super().__init__()
         self.embed = embed
         
     @discord.ui.button(label='Yes', emoji="😎", style=discord.ButtonStyle.green)
-    async def count_is_going(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def add_yes(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Add user name into message, if user clicked on 'yes' button.
 
+        Args:
+            interaction (discord.Interaction): Button interaction.
+            button (discord.ui.Button): Discord button.
+        """
         user = str(interaction.user).split("#")[0]
         CONFIRMED.add(user)
         if user in DECLINED:
             DECLINED.remove(user)
         elif user in MAYBE:
             MAYBE.remove(user)
-        self.embed.set_field_at(index=2, name="Confirmed:", value='\n'.join(CONFIRMED))
-        self.embed.set_field_at(index=3, name="Declined:", value='\n'.join(DECLINED))
-        self.embed.set_field_at(index=4, name="Maybe:", value='\n'.join(MAYBE))
+        await self._set_fields()
         await interaction.response.edit_message(embed=self.embed)
         
     @discord.ui.button(label='No', emoji="😢", style=discord.ButtonStyle.red)
-    async def count_is_not_going(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def add_no(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Add user name into message, if user clicked on 'no' button.
+
+        Args:
+            interaction (discord.Interaction): Button interaction.
+            button (discord.ui.Button): Discord button.
+        """
 
         user = str(interaction.user).split("#")[0]
         DECLINED.add(user)
@@ -42,13 +152,17 @@ class ButtonView(discord.ui.View):
             CONFIRMED.remove(user)
         elif user in MAYBE:
             MAYBE.remove(user)
-        self.embed.set_field_at(index=2, name="Confirmed:", value='\n'.join(CONFIRMED))
-        self.embed.set_field_at(index=3, name="Declined:", value='\n'.join(DECLINED))
-        self.embed.set_field_at(index=4, name="Maybe:", value='\n'.join(MAYBE))
+        await self._set_fields()
         await interaction.response.edit_message(embed=self.embed)
         
     @discord.ui.button(label='Maybe', emoji="🤔", style=discord.ButtonStyle.gray)
-    async def count_is_maybe_going(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def add_maybe(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Add user name into message, if user clicked on 'maybe' button.
+
+        Args:
+            interaction (discord.Interaction): Button interaction.
+            button (discord.ui.Button): Discord button.
+        """
 
         user = str(interaction.user).split("#")[0]
         MAYBE.add(user)
@@ -56,70 +170,49 @@ class ButtonView(discord.ui.View):
             CONFIRMED.remove(user)
         elif user in DECLINED:
             DECLINED.remove(user)
+        await self._set_fields()
+        await interaction.response.edit_message(embed=self.embed)
+        
+    async def _set_fields(self):
         self.embed.set_field_at(index=3, name="Declined:", value='\n'.join(DECLINED))
         self.embed.set_field_at(index=2, name="Confirmed:", value='\n'.join(CONFIRMED))
         self.embed.set_field_at(index=4, name="Maybe:", value='\n'.join(MAYBE))
-        await interaction.response.edit_message(embed=self.embed)
         
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="$", case_insensitive=True, strip_after_prefix=True, intents=intents)
 
 @bot.command()
 async def vote(ctx, *args):
-    if "--from" in args:
-        from_date = re.findall(r'\d+', args[args.index("--from")+1])
-        from_date.reverse()
-        from_date = date(*[int(i) for i in from_date])
-    else:
-        from_date = None
-    if "--to" in args:
-        to_date = re.findall(r'\d+', args[args.index("--to")+1])
-        to_date.reverse()
-        to_date = date(*[int(i) for i in to_date])
-    else:
-        to_date = None
-    calendar = await create_calendar(from_date, to_date)
+    """Create voting calendar, when command 'vote' appears in messages.
+
+    Args:
+        ctx (discord.ext.commands.Context): Message context (metadata).
+    """
+    planner = SessionPlanner()
+    await planner.set_calendar(*args)
+    calendar = await planner.get_calendar()
     embed = discord.Embed(title="Vote fot next session date!", color=discord.Color.dark_purple())
     await ctx.channel.send(embed=embed)
-    for day in calendar:
-        embed = discord.Embed(description=day, color=discord.Color.dark_purple())
+    for embed in calendar:
         message = await ctx.channel.send(embed=embed)
         await message.add_reaction("✅")
         await message.add_reaction("❔")
         await message.add_reaction("❌")
-
-async def create_calendar(from_date, to_date):
-    planner = SessionPlanner()
-    planner.set_calendar(from_date, to_date)
-    return planner.get_calendar()
-
+        
 @bot.command()
 async def plan(ctx, *args):
-    if "--date" in args:
-        session_date = args[args.index("--date")+1]
-    else:
+    """Create session event, when command 'vote' appears in messages.
+
+    Args:
+        ctx (discord.ext.commands.Context): Message context (metadata).
+    """
+    planner = SessionPlanner()
+    if not await planner.set_session(*args):
         await ctx.channel.send("Please, give me date of the session.")
         return
-    if "--name" in args:
-        session_name = args[args.index("--name")+1]
-    else:
-        session_name = "Next session"
-    if "--place" in args:
-        session_place = args[args.index("--place")+1]
-    else:
-        session_place = "Somewhere"
-    embed = discord.Embed(title=session_name, description="Are you going?", color=discord.Color.dark_purple())
-    embed.add_field(name="When:", value=session_date, inline=False)
-    embed.add_field(name="Where:", value=session_place, inline=False)
-    embed.add_field(name="Confirmed:", value="", inline=False)
-    embed.add_field(name="Declined:", value="", inline=False)
-    embed.add_field(name="Maybe:", value="", inline=False)
+    embed = await planner.get_session()
     button = ButtonView(embed)
     message = await ctx.channel.send(embed=embed, view=button)
     await message.pin()
-    
+
     
 if __name__ == "__main__":
     bot.run(os.getenv('DISCORD_TOKEN'))
